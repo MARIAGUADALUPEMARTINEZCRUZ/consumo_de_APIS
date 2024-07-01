8ee37e82ecbffca5aa51677cc1f6849d4d3d88e7
Lupita Martinez <21300055@uttt.edu.mx>
2024-07-01 14:17:52 -0600

    apis

--- get_interfaces.py
+++ get_interfaces.py
@@ -1,108 +1,112 @@
 import json
 import requests
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 requests.packages.urllib3.disable_warnings()
 
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
 
+api_url = "https://192.168.56.101/restconf/"
+headers = {
+    "Accept": "application/yang-data+json",
+    "Content-type": "application/yang-data+json"
+}
+basicauth = ("cisco", "cisco123!")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/interfaces')
 def get_interfaces():
     module = "data/ietf-interfaces:interfaces"
     resp = requests.get(f'{api_url}{module}', auth=basicauth, headers=headers, verify=False)
-    print(json.dumps(resp.json(), indent=4))
     data_json = resp.json()
+    interfaces = []
+
     if resp.status_code == 200:
-        for key, valor in data_json.items():
-            print(f'Nombre de la interface: {valor["interface"][1]["name"]}')
-            print(f'Descripción de la interface: {valor["interface"][1]["description"]}')
-            print(f'Status de la interface: {valor["interface"][1]["enabled"]}')
-    else:
-        print(f'Error al realizar la consulta del modulo {module}')
+        for interface in data_json["ietf-interfaces:interfaces"]["interface"]:
+            interfaces.append({
+                'name': interface['name'],
+                'ip':['ip'],
+                'description': interface.get('description', 'N/A'),
+                'enabled': interface['enabled']
+            })
+    return render_template('interfaces.html', interfaces=interfaces)
 
 
-def get_restconf_native():
-    module = "data/Cisco-IOS-XE-native:native"
-    resp = requests.get(f'{api_url}{module}', auth=basicauth, headers=headers, verify=False)
-    if resp.status_code == 200:
-        print(json.dumps(resp.json(), indent=4))
-    else:
-        print(f"Error al consumir la API para el modulo {module}")
-
-
-def banner_motd():
+@app.route('/banner')
+def get_banner():
     module = "data/Cisco-IOS-XE-native:native/banner/motd"
     resp = requests.get(f'{api_url}{module}', auth=basicauth, headers=headers, verify=False)
+    banner = ""
+
     if resp.status_code == 200:
-        print(json.dumps(resp.json(), indent=4))
-    else:
-        print(f"Error al consumir la API para el modulo {module}")
+        banner = resp.json().get("Cisco-IOS-XE-native:motd", {}).get("banner", "")
+
+    return render_template('banner.html', banner=banner)
 
 
-def put_banner():
-    banner = {
-        "Cisco-IOS-XE-native:motd": {
-            "banner": "#Solo tienen permiso los priviligiados"
-        }
-    }
-
-    module = "data/Cisco-IOS-XE-native:native/banner/motd"
-    resp = requests.put(f'{api_url}{module}', data=json.dumps(banner), auth=basicauth, headers=headers, verify=False)
-    print(resp.status_code)
-    if resp.status_code == 204:
-        print("Actualización exitosa")
-    else:
-        print(f"Error, no se puede realizar la actualización al modulo{module}")
-
-
+@app.route('/agregar', methods=['GET', 'POST'])
 def post_loopback():
-    dloopback = json.dumps({
-        "ietf-interfaces:interface": {
-            "name": "Loopback100",
-            "description": "Configured by RESTCONF",
-            "type": "iana-if-type:softwareLoopback",
-            "enabled": True,
-            "ietf-ip:ipv4": {
-                "address": [
-                    {
-                        "ip": "172.16.100.1",
-                        "netmask": "255.255.255.0"
-                    }
-                ]
+    if request.method == 'POST':
+        loopback_name = request.form['nombre']
+        ip = request.form['ip']
+        netmask = request.form['mascara']
+        descripcion = request.form['descripción']
+
+        dloopback = {
+            "ietf-interfaces:interface": {
+                "name": loopback_name,
+                "description": descripcion,
+                "type": "iana-if-type:softwareLoopback",
+                "enabled": True,
+                "ietf-ip:ipv4": {
+                    "address": [
+                        {
+                            "ip": ip,
+                            "netmask": netmask
+                        }
+                    ]
+                }
             }
         }
-    })
 
-    module = "data/ietf-interfaces:interfaces"
-    resp = requests.post(f'{api_url}{module}', auth=basicauth, headers=headers, data=dloopback, verify=False)
-    if resp.status_code == 201:
-        print(f"Se insertó correctamente {dloopback}")
-    else:
-        print(f"Error al insertar elemento al módulo{module}")
+        module = "data/ietf-interfaces:interfaces"
+        resp = requests.post(f'{api_url}{module}', auth=basicauth, headers=headers, data=json.dumps(dloopback), verify=False)
+
+        if resp.status_code == 201:
+            flash(f"Loopback: {loopback_name} agregado exitosamente!", "success")
+            return json.dumps({'status': 'success', 'message': f"Loopback: {loopback_name} agregado exitosamente!"}), 201
+        else:
+            flash(f"Error al agregar Loopback {loopback_name}", "danger")
+            return json.dumps(
+                {'status': 'error', 'message': f"Error al agregar Loopback: {loopback_name}"}), resp.status_code
+
+    return render_template('agregar.html')
 
 
+@app.route('/eliminar', methods=['GET', 'POST'])
 def del_loopback():
-    module = "data/ietf-interfaces:interfaces/interface=Loopback100"
-    dloopback = {}
-    resp = requests.delete(f'{api_url}{module}', auth=basicauth, headers=headers, data=dloopback, verify=False)
-    if resp.status_code == 204:
-        print(f"Se eliminó correctamente el elemento")
-        get_interfaces()
-    else:
-        print(f"Error al eliminar el elemento del módulo{module}")
+    if request.method == 'POST':
+        loopback_name = request.form['nombre']
+
+        module = f"data/ietf-interfaces:interfaces/interface={loopback_name}"
+        resp = requests.delete(f'{api_url}{module}', auth=basicauth, headers=headers, verify=False)
+
+        if resp.status_code == 204:
+            flash(f"Loopback {loopback_name} eliminado exitosamente!", "success")
+            return json.dumps({'status': 'success', 'message': f"Loopback {loopback_name} eliminado exitosamente!"}), 204
+        else:
+            flash(f"Error al eliminar Loopback {loopback_name}", "danger")
+            return json.dumps(
+                {'status': 'error', 'message': f"Error al eliminar Loopback {loopback_name}"}), resp.status_code
+
+    return redirect(url_for('eliminar.html'))
 
 
 if __name__ == '__main__':
-    # module:operations, data
-    api_url = "https://192.168.56.101/restconf/"
-    headers = {"Accept": "application/yang-data+json",
-               "Content-type": "application/yang-data+json"
-               }
-    basicauth = ("cisco", "cisco123!")
-
-    get_interfaces()
-    #get_restconf_native()
-    #banner_motd()
-    #put_banner()
-    #post_loopback()
-    #del_loopback()
-
-    # Diseñar interfaces gráficas amigables que pemitan hacer estaa pues este comsumo de las apis,
-    # agregar dos elementos diferentes de IOS-XE-native en HTML o en programa de escritorio
+    app.run(debug=True)
