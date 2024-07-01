79bac66a280ff9bfccb50a2272f20821831c853a
Lupita Martinez <21300055@uttt.edu.mx>
2024-07-01 14:52:53 -0600

    apis

--- get_interfaces.py
+++ get_interfaces.py
@@ -31,7 +31,7 @@ def get_interfaces():
         for interface in data_json["ietf-interfaces:interfaces"]["interface"]:
             interfaces.append({
                 'name': interface['name'],
-                'ip':['ip'],
+                'ip': interface.get('ietf-ip:ipv4', {}).get('address', [{}])[0].get('ip', 'N/A'),
                 'description': interface.get('description', 'N/A'),
                 'enabled': interface['enabled']
             })
@@ -89,13 +89,17 @@ def post_loopback():
     return render_template('agregar.html')
 
 
-@app.route('/eliminar', methods=['GET','POST'])
+@app.route('/eliminar', methods=['GET', 'POST'])
 def del_loopback():
     if request.method == 'POST':
         loopback_name = request.form['nombre']
 
+        # Intentar eliminar la interfaz
         module = f"data/ietf-interfaces:interfaces/interface={loopback_name}"
+        print(f"Deleting loopback interface: {module}")  # Debug statement
         resp = requests.delete(f'{api_url}{module}', auth=basicauth, headers=headers, verify=False)
+        print(f"Response status code: {resp.status_code}")  # Debug statement
+        print(f"Response content: {resp.content}")  # Debug statement
 
         if resp.status_code == 204:
             flash(f"Loopback {loopback_name} eliminado exitosamente!", "success")
@@ -106,6 +110,7 @@ def del_loopback():
                 {'status': 'error', 'message': f"Error al eliminar Loopback {loopback_name}"}), resp.status_code
 
     return render_template('eliminar.html')  # Renderiza la plantilla 'eliminar.html'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
